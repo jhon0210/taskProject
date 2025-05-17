cb529ce9e3a542e95c835022235450d3ab3afa8e
jhon0210 <jhondavidr56@gmail.com>
2025-05-16 21:52:50 -0500

    [feat]: implementa middleware para envio de datos JSON y mapeo de rutas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api', tasksRoutes);
 
 app.use(morgan('dev'));
 
