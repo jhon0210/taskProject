990234cd247e9eb01f1bc58a846c10743117e246
jhon0210 <jhondavidr56@gmail.com>
2025-05-16 20:43:10 -0500

    [feat]: implementa las rutas con los endpoints y metodos para el CRUD

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,22 @@
+import {Router} from 'express';
+import {
+    getTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+} from '../controllers/tasks.controller.js';
+
+const router = Router();
+
+router.get('/tasks', getTasks);
+
+router.get('/tasks/:id', getTask);
+
+router.post('/tasks', createTask);
+
+router.delete('/tasks/:id', deleteTask);
+
+router.put('/tasks/:id', updateTask);
+
+export default router;
