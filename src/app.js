8e00568ba249a5714477384a710b5af61548fa5c
jhon0210 <jhondavidr56@gmail.com>
2025-05-17 23:22:21 -0500

    [feat]: implementa middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    
+}));
 
 app.use(express.json());
 
