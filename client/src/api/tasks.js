f507f4fb92a275c5cf11d1f0f27cf72b711d37f7
jhon0210 <jhondavidr56@gmail.com>
2025-05-17 23:11:51 -0500

    [feat]: implementa los metodos para el crud con axios

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -6,6 +6,6 @@ export const getTaskRequest = async (id) => axios.get(`/tasks/${id}`);
 
 export const createTasksRequest = async (task) => axios.post('/tasks', task);
 
-export const updateTasksRequest = async (task) => axios.put(`/tasks/${task._id}`, task);
+export const updateTasksRequest = async (id, task) => axios.put(`/tasks/${id}`, task);
 
 export const deleteTasksRequest = async (id) => axios.delete(`/tasks/${id}`);
