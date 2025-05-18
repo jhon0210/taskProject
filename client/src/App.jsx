9ee07a76c17481671393cc3c77480b6a0dee5dc1
jhon0210 <jhondavidr56@gmail.com>
2025-05-17 23:07:34 -0500

    [feat]: componente BrowserRouter para navegacion entre paginas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import HomePage from "./pages/HomePage";
+import TasksPage from "./pages/TasksPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import { TaskProvider } from "./context/TasksContext";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/tasks" element={<TasksPage/>} />
-        <Route path="/add-task" element={<TaskFormPage/>} />
-        <Route path="/tasks/:id" element={<TaskFormPage/>} />
-      </Routes>
-    </BrowserRouter>
+    <TaskProvider>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/add-task" element={<TaskFormPage />} />
+          <Route path="/tasks/:id" element={<TaskFormPage />} />
+        </Routes>
+      </BrowserRouter>
+    </TaskProvider>  
   )
 }
 
