e4ca51ea046bcd15850e460b4242a5ba1e0fdd67
jhon0210 <jhondavidr56@gmail.com>
2025-05-17 23:15:15 -0500

    [feat]: implementa funciones get,create,update,delete

--- client/src/context/TasksContext.jsx
+++ client/src/context/TasksContext.jsx
@@ -1,4 +1,12 @@
 import { createContext, useContext, useState } from "react";
+import { 
+    createTasksRequest, 
+    getTasksRequest, 
+    deleteTasksRequest,
+    getTaskRequest,
+    updateTasksRequest
+ } from "../api/tasks";
+import { get } from "mongoose";
 
 const TaskContext = createContext();
 
@@ -13,15 +21,51 @@ export const useTasks = () => {
 export function TaskProvider({ children }) {
     const [tasks, setTasks] = useState([]); 
     
-    const createTask = (task) => {
-        console.log('hola task');
+    const getTasks = async () => {
+        const res = await getTasksRequest();
+        setTasks(res.data);
+    }
+    const createTask = async (task) => {
+       const res = await createTasksRequest(task);
+       console.log(res);
+    }
+
+    const deleteTask = async (id) => {
+        try {
+            const res = await deleteTasksRequest(id);
+            if (res.status === 204) setTasks(tasks.filter(task => task._id !== id));
+        } catch (error) {
+            console.log(error);     
+            
+        }
+    }
+
+    const getTask = async (id) => {
+        try {
+            const res = await getTaskRequest(id);
+            return res.data;
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const updateTask = async (id, task) => {
+        try {
+            await updateTasksRequest(id, task);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
         <TaskContext.Provider 
             value={{
                 tasks,
-                createTask 
+                createTask,
+                getTasks,
+                deleteTask,
+                getTask,
+                updateTask 
             }}
         >
             {children}
